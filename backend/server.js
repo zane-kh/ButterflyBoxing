164905d6dabe9d9b0802da7e666ddc2b36cce66c
Zane <zkhimjisgu@gmail.com>
2023-01-11 14:53:28 -0800

    mongoose connect replace method

--- backend/server.js
+++ backend/server.js
@@ -69,15 +69,11 @@ app.use(errorHandler);
 //connect to atlas database
 import mongoose from "mongoose";
 
-// const DB = process.env.MONGO_URI.replace("<PASSWORD>", process.env.PASSWORD);
+const DB = process.env.MONGO_URI.replace("<PASSWORD>", process.env.PASSWORD);
 
-mongoose
-  .connect(
-    "mongodb+srv://zane:password1234@cluster0.f2d7l.mongodb.net/ButterflyBoxing?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("DB connection successful!".cyan.underline);
-  });
+mongoose.connect(DB).then(() => {
+  console.log("DB connection successful!".cyan.underline);
+});
 //
 
 import colors from "colors";
